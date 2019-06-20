8e6de4b4768f0c4fb258a24b40c8d42aca6b1605
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-20 18:41:09 +0200

    Enter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button';
 
 function App() {
   return (
     <div className="App" id="quote-box">
-    
+      <Button buttonDisplayName="Next Quote" />
     </div>
   );
 }
--- src/components/button.js
+++ src/components/button.js
@@ -3,3 +3,5 @@ import React from 'react'
 const Button = ({ buttonDisplayName }) => (
     <button>{buttonDisplayName}</button>
 );
+
+export default Button;
