a2a6bc5d6d884ffeb27e918f5fefdff40e9332a7
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-20 20:09:52 +0200

    Enter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Button from './components/button';
 
-function App() {
-  return (
-    <div className="App" id="quote-box">
-      <Button buttonDisplayName="Next Quote" />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes = [],
+      selectedQuoteIndex: null
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/shreyasminocha/7d5dedafc1fe158f82563c1223855177/raw/325d51aca7165b2498971afcff9bed286a52dc0e/quotes.json')
+      .then(data => data.json())
+      .then(d => console.log(d))
+  }
+  
+  nextQuoteClickHandler() {
+    console.log('Hi');
+  }
+
+  render() {
+    return (
+      <div className="App" id="quote-box">
+        <Button buttonDisplayName="Next Quote" clickHandler={this.nextQuoteClickHandler}/>
+      </div>
+    );
+  }
 }
 
 export default App;
