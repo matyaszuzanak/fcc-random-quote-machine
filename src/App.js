5806b9a3ee26f00e9f50a9dcc44039287d618314
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-20 20:22:17 +0200

    Enter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes = [],
+      quotes: [],
       selectedQuoteIndex: null
     }
   }
@@ -14,11 +14,11 @@ class App extends Component {
   componentDidMount() {
     fetch('https://gist.githubusercontent.com/shreyasminocha/7d5dedafc1fe158f82563c1223855177/raw/325d51aca7165b2498971afcff9bed286a52dc0e/quotes.json')
       .then(data => data.json())
-      .then(d => console.log(d))
+      .then(quotes => this.setState({ quotes }));
   }
   
-  nextQuoteClickHandler() {
-    console.log('Hi');
+  selectedQuoteIndex() {
+    
   }
 
   render() {
