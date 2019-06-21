7af5341d5be92479a345f901645784704be8b1a1
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-21 13:39:37 +0200

    enter

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import Button from './Button';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { fontAwesomeIcon, FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const QuoteMachine = (props) => (
-    <>
-        { props.selectedQuote ? `"${props.selectedQuote.quote}" - ${props.selectedQuote.author}` : '' }
-        <Button buttonDisplayName="Next Quote" clickHandler={props.assignNewQuoteIndex} />
-    </>
+    <Card>
+        <CardContent>
+           { 
+                props.selectedQuote ?
+                (
+                    <Typography>
+                        {props.selectedQuote.quote} - {props.selectedQuote.author}
+                    </Typography>
+                ) : null
+            }    
+        </CardContent>
+        <CardActions>
+            <Button size="small" onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+            <IconButton>
+                <FontAwesomeIcon> icon={faTwitter} size="md"</FontAwesomeIcon>
+            </IconButton>
+        </CardActions>
+        
+    </Card>
 );
 
 export default QuoteMachine;
